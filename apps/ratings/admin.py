f83f33672539646a647689fa35cb366857b626a4
Than Trung Tam <n22dcat047@student.ptithcm.edu.vn>
2024-05-21 21:26:47 +0700

    features: add rating models and serializers

--- apps/ratings/admin.py
+++ apps/ratings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+class RatingAdmin(admin.ModelAdmin):
+    #hien thi danh sach trong truong admin
+    list_display = ["rater", "agent", "rating"]
+
+admin.site.register(Rating, RatingAdmin)
